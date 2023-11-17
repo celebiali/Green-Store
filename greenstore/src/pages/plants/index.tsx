ae38903fb6929fc39bcd336c4f3d413bb37bef78
Ali Rıza ÇELEBİ <ali1999_@hotmail.com>
2023-11-17 21:40:10 +0300

    product page added

--- greenstore/src/pages/plants/index.tsx
+++ greenstore/src/pages/plants/index.tsx
@@ -1,16 +1,39 @@
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
+import Product from "../../components/product/Product";
 
 function Plants() {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="plants_container">
       <Helmet>
         <title>
           {t("navbar.green-store")} - {t("navbar.plants")}
         </title>
       </Helmet>
-      <div>{t("navbar.plants")}</div>
+      <div className="plants_products">
+        <div className="plants">
+          <div className="breadcrump">Home / Plants</div>
+          <div className="product_filter">
+            <div className="product_filter_text">Showing 1–9 of 25 results</div>
+            <div className="product_filter_select">Default sorting ^</div>
+          </div>
+          <div className="product-container">
+            <div className="product_card">
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+            </div>
+            <div className="product_card">
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+            </div>
+            <div className="product_card">
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+              <Product title="Title" price="$123" brand="Brand" star="xxxx" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
