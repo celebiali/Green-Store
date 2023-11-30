71a110cfa89fd26d3f5b87c7cae946c042541b6f
Ali Rıza ÇELEBİ <ali1999_@hotmail.com>
2023-11-30 23:00:50 +0300

    login register modal refactoring

--- greenstore/src/components/modals/auth/login/Login.tsx
+++ greenstore/src/components/modals/auth/login/Login.tsx
@@ -2,17 +2,64 @@ import { createModal, destroyModal } from "../../../../stores/Modals/actions";
 
 export default function Login() {
   return (
-    <div style={{ width: 500 }}>
-      Login <br />
-      <button
-        onClick={() => {
-          destroyModal();
-          createModal("register");
-        }}
+    <div
+     className="login_modal"
+    >
+      <div
+      className="header"
       >
-        Register Modalı Yükle
-      </button>
-      <button onClick={destroyModal}>Kapat</button>
+        <span>Giriş yap</span>
+        <span onClick={destroyModal}>X</span>
+      </div>
+      <hr />
+      <div
+     className="container"
+      >
+        <div>
+          <label>User name</label>
+        </div>
+        <div style={{ marginTop: 5 }}>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            required
+          className="username_input"
+          />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <label>Password</label>
+        </div>
+        <div style={{ marginTop: 5 }}>
+          <input
+            type="text"
+            placeholder="Password"
+            name="uname"
+            required
+           className="password_input"
+          />
+        </div>
+        <div style={{ marginTop: 10, marginBottom: 20 }}>
+          <button type="submit" style={{ marginLeft: 0 }}>
+            Giriş Yap
+          </button>
+        </div>
+        <hr />
+        <div
+        className="isUser"
+        >
+          <span>Hesabın yok mu?</span>
+          <a
+            href="#"
+            style={{ marginLeft: 5, color: "black" }}
+            onClick={() => {
+              destroyModal();
+              createModal("register");
+            }}
+          >
+            Kayıt Ol
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- greenstore/src/components/modals/auth/register/Register.tsx
+++ greenstore/src/components/modals/auth/register/Register.tsx
@@ -1,10 +1,68 @@
-import { destroyModal } from "../../../../stores/Modals/actions";
+import { createModal, destroyModal } from "../../../../stores/Modals/actions";
 
 export default function Register() {
   return (
-    <div>
-      Register
-      <button onClick={destroyModal}>Kapat</button>
+    <div className="register_modal">
+      <div className="header">
+        <span>Kayıt Ol</span>
+        <span onClick={destroyModal}>X</span>
+      </div>
+      <hr />
+      <div className="container">
+        <div>
+          <label>Kullanıcı Adı</label>
+        </div>
+        <div style={{ marginTop: 5 }}>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            required
+            className="username_input"
+          />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <label>E-Posta Adresi </label>
+        </div>
+        <div style={{ marginTop: 5 }}>
+          <input
+            type="text"
+            placeholder="Enter E-Mail"
+            required
+            className="email_input"
+          />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <label>Password</label>
+        </div>
+        <div style={{ marginTop: 5 }}>
+          <input
+            type="text"
+            placeholder="Password"
+            name="uname"
+            required
+            className="password_input"
+          />
+        </div>
+        <div style={{ marginTop: 10, marginBottom: 20 }}>
+          <button type="submit" style={{ marginLeft: 0 }}>
+            Kayıt Ol
+          </button>
+        </div>
+        <hr />
+        <div className="isUser">
+          <span>Hesabın var mı?</span>
+          <a
+            href="#"
+            style={{ marginLeft: 5, color: "black" }}
+            onClick={() => {
+              destroyModal();
+              createModal("login");
+            }}
+          >
+            Giriş Yap
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
