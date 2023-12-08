83fad2bdecda19473ef6c0b6a3f34e39fa68fbdc
alicelebi <ali1999_@hotmail.com>
2023-12-08 18:18:59 +0300

    refactoring

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const { authenticationMid } = require("../middleware/auth");
 
 const router = express.Router();
 
+
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
